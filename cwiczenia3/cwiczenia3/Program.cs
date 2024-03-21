f256948b44ebb4447149d3d6c92133f899cfc51b
TheBitson124 <as.sosnowicz@gmail.com>
2024-03-21 19:50:40 +0100

    Adding main Program

--- cwiczenia3/cwiczenia3/Program.cs
+++ cwiczenia3/cwiczenia3/Program.cs
@@ -4,15 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using cwiczenia3.Containers;
-
-
+using cwiczenia3.Ships;
 namespace cwiczenia3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Ship
+            Ship ship = new Ship(100.0, 5, 1000);
+            LiquidContainer lc = new LiquidContainer(100, 5, 5, 5, 'C',150, "biezpieczny");
+            GasContainer gc = new GasContainer(100, 5,,5,'D',150,50.0);
+            CoolingContainer cc = new CoolingContainer(100, 10, 7, 8, 150, PossibleProducts.Bananas, 50);
+            ship.LoadContainer(lc);
+            ship.LoadContainer(gc);
+            ship.newLiquidContainer(5, 10, 8, 'C', 200, "niebezpieczny");
+
+
         }
 
     }
